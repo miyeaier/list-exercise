a055b771c0081f489185bcd053482fdc7dd3edc9
miyeaier <miyesier@gmail.com>
2022-02-28 10:44:09 +0100

    add cypress test for employee items

--- cypress/integration/displayEmployeeList.js
+++ cypress/integration/displayEmployeeList.js
@@ -2,7 +2,20 @@ describe('Display list of employees', () => {
   beforeEach(() => {
     cy.visit('/')
   });
-  it('displays a header', () => {
+  it('it expect displays a header', () => {
     cy.get('#header').should('contain', 'Employee List')
   });
+
+  it('it expect displays a list with 6 items', () => {
+    cy.get("#employee-list").within(() => {
+      cy.get('.employee-item').should('have.length', 6)
+    })
+
+    it('it expect the list items display the expected content',() => {
+      cy.get('#employee-item')
+      .frist()
+      .should('contain','George Bluth')
+      
+    });
+  })
 });
